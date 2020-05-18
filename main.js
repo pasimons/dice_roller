7fdb12c865d22bf0d00c0d4e0f9536b7851d0f9d
patricia simons <trishasimons16@gmail.com>
2020-05-18 13:15:53 -0400

    save

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
-let roll = document.querySelector('#roll');
-let showAll = document.querySelector('#show-all');
-let numberOfDiceroll = document.querySelector('#number-of-diceroll');
-
-
-
-roll.addEventListener("click", function(){
-    console.log("dice roll button clicked");
-    let newRoll=Number(roll.innerHTML) +1;
-    diceRoll.innerHTML =newRoll ;
+let diceRollDisplay = document.querySelector('#dice-roll-display');
+diceRollDisplay.addEventListener("click", function(){
+    console.log("dice-roll-display");
 })
+
+
+let roll = document.querySelector('roll');
+roll.addEventListener("click", function(){
+    console.log('roll');
+})
+
+
+// let dieRolls = document.querySelector('#dieRolls');
+// dieRolls.addEventListener("click", function(){
+//     console.log("dieRolls");
+// })
+
+var dieRolls = new Array();
+
+
+let showAll = document.querySelector('#show-All');
+showAll.addEventListener("click", function(){
+    console.log("show-all");
+})
+
+
+let numberOfDiceRoll = document.querySelector('number-of-dice-roll');
+numberOfDiceRoll.addEventListener("click", function(){
+    console.log("number-of-dice-roll");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
